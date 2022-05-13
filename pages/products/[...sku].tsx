1f578c2a5dfd53750d160583de9a55bece7102a1
Markus Andersson <markus.andersson_93@hotmail.com>
2022-05-13 11:27:28 +0200

    baseUrl mockPrices

--- pages/products/[...sku].tsx
+++ pages/products/[...sku].tsx
@@ -1,9 +1,62 @@
-import { NextPage } from "next";
+import {
+  GetServerSideProps,
+  InferGetServerSidePropsType,
+  NextPage,
+} from "next";
+import { getPrices } from "api/priceApi";
+import { Price } from "types/price";
 
-type Props = {};
+const Product: NextPage<
+  InferGetServerSidePropsType<typeof getServerSideProps>
+> = ({ prices }) => {
+  return (
+    <div>
+      PRICEs:{" "}
+      {prices &&
+        prices.map(
+          (
+            {
+              recommendedRetailPrice,
+              priceAmount,
+              currency,
+              discount,
+              discountAmount,
+            },
+            index
+          ) => (
+            <p key={`price-$index`}>
+              {`Pris: ${priceAmount} ${currency}`}
+              <br />
+              {`Rek: ${recommendedRetailPrice} ${currency}`}
+              <br />
+              {`Rabatt: ${discountAmount} ${currency}`}
+              <br />
+              {`Rabatt %: ${discount}`}
+              <br />
+            </p>
+          )
+        )}
+    </div>
+  );
+};
 
-const Product: NextPage<Props> = () => {
-  return <div>PRICE CALCULATOR PRODUCT</div>;
+type Props = { prices: Price[] };
+
+export const getServerSideProps: GetServerSideProps<Props> = async ({
+  params,
+}) => {
+  const sku = params?.sku as string;
+  if (!sku) {
+    return {
+      notFound: true,
+    };
+  }
+
+  const prices = getPrices([sku], "");
+
+  return {
+    props: { prices },
+  };
 };
 
 export default Product;
