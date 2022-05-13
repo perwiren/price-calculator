8f7afcdb51c6560c0f3ed5ad1e10004cb4b7d317
per-wiren <per.wiren@ahlens.se>
2022-05-13 11:25:32 +0200

    Removed stuff

--- pages/products/[...sku].tsx
+++ pages/products/[...sku].tsx
@@ -3,11 +3,7 @@ import { getPrices } from "../../api/priceApi";
 
 type Props = {};
 
-
-
 const Product: NextPage<Props> = () => {
-  const prices = getPrices(["ABC123"], "QWE");
-  console.log("got prices", prices)
   return <div>PRICE CALCULATOR PRODUCT</div>;
 };
 
