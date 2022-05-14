3fe29fbd336c28ba56f1d65da54d7a3ae4acd0d9
per-wiren <per.wiren@ahlens.se>
2022-05-14 10:00:50 +0200

    Added margin

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -7,7 +7,7 @@ type Props = { children: React.ReactNode };
 const Layout: React.FC<Props> = ({ children }) => (
   <div className="flex flex-col h-screen justify-between">
     <Header />
-    <main className="container mb-auto mt-4 mx-auto px-4">
+    <main className="container mb-auto mt-4 mb-4 mx-auto px-4">
       {children}
     </main>
     <Footer />
