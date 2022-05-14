04d23efa7f53feae4d5ca31c86898155da046195
per-wiren <per.wiren@ahlens.se>
2022-05-14 10:27:23 +0200

    Call API for SKU

--- api/priceApi.ts
+++ api/priceApi.ts
@@ -1,20 +1,36 @@
 import { mockPrices } from "../data/mocks/mockPrices";
 import { IPrice } from "../types/price";
 
+const URL = "https://dometicservices.azurewebsites.net/";
+
 export const getPrices = async (skus: string[], priceCode: string): Promise<IPrice[]> => {
-    // try {
-    //     const result = await fetch("/", { method: "POST" });
-    //     const prices = await result.json();
-    //     return prices;
-    // } catch (e) {
-    //     return Promise.reject(e);
-    // }
-    return mockPrices.filter(mp => skus.some(sku => sku === mp.sku));
+    try {
+        const payload =
+        {
+            method: "POST", 
+            headers: {
+                'Accept': 'application/json, text/plain',
+                'Content-Type': 'application/json;charset=UTF-8'
+            },
+            body: JSON.stringify(
+                {
+                    skus: skus,
+                    priceCode: "CG1"
+                })
+        };
+        console.log(payload)
+        const result = await fetch(URL, payload);
+        const prices = await result.json();
+        console.log('prices', prices)
+        return prices;
+    } catch (e) {
+        return Promise.reject(e);
+    }
 }
 
 export const getAllPrices = async (): Promise<IPrice[]> => {
     try {
-        const result = await fetch("https://dometicservices.azurewebsites.net/", { method: "GET" });
+        const result = await fetch(URL, { method: "GET" });
         const prices = await result.json();
         return prices;
     } catch (e) {
